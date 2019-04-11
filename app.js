542c9431e2678fdaa06b1bfefbd8bedc0cf6e7c9
Weslley Braga <wesbragagt@gmail.com>
2019-04-11 10:27:24 -0500

    working product changing stock quantity if the user decides to buy

--- app.js
+++ app.js
@@ -11,52 +11,73 @@ var connection = mysql.createConnection({
     password: login.password,
     database: login.database
 });
+
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connection made with server at " + connection.threadId);
+    initShop();
+    function initShop() {
+        connection.query(
+            "SELECT item_id, product_name, price FROM rockinzon_db.products",
+            function(err, result) {
+                if (err) throw err;
+                inquirer
+                    .prompt({
+                        type: "rawlist",
+                        name: "choice",
+                        message: "what would you like to purchase?",
+                        choices: function() {
+                            var choicesArr = [];
+                            for (var i = 0; i < result.length; i++) {
+                                choicesArr.push(result[i].product_name);
+                            }
+                            return choicesArr;
+                        }
+                    })
+                    .then(function(answers) {
+                        var productChosen = answers.choice;
+                        connection.query(
+                            "SELECT product_name, price, stock_quantity FROM rockinzon_db.products WHERE product_name=?",
+                            [productChosen],
+                            function(err, res) {
+                                if (err) throw err;
+                                var itemName = res[0].product_name;
+                                var itemPrice = "$" + res[0].price + ".00";
+                                inquirer
+                                    .prompt([
+                                        {
+                                            type: "list",
+                                            name: "choice",
+                                            message:
+                                                "Would you like to purchase a " +
+                                                itemName +
+                                                " for " +
+                                                itemPrice +
+                                                "?",
+                                            choices: ["Yes", "No"]
+                                        }
+                                    ])
+                                    .then(function(answers) {
+                                        var confirm = answers.choice;
 
-    connection.query(
-        "SELECT item_id, product_name, price FROM bamazon_db.products",
-        function(err, result) {
-            if (err) throw err;
-            inquirer
-                .prompt({
-                    type: "rawlist",
-                    name: "choice",
-                    message: "what would you like to purchase?",
-                    choices: function() {
-                        var choicesArr = [];
-                        for (var i = 0; i < result.length; i++) {
-                            choicesArr.push(result[i].product_name);
-                        }
-                        return choicesArr;
-                    }
-                })
-                .then(function(answers) {
-                    var productChosen = answers.choice;
-                    connection.query(
-                        "SELECT product_name, price, stock_quantity FROM bamazon_db.products WHERE product_name=?",
-                        [productChosen],
-                        function(err, res) {
-                            if (err) throw err;
-                            var itemName = res[0].product_name;
-                            var itemPrice = "$" + res[0].price + ".00";
-                            inquirer.prompt([
-                                {
-                                    type: "list",
-                                    name: "choice",
-                                    message:
-                                        "Would you like to purchase a " +
-                                        itemName +
-                                        " for " +
-                                        itemPrice +
-                                        "?",
-                                    choices: ["Yes", "No"]
-                                }
-                            ]);
-                        }
-                    );
-                });
-        }
-    );
+                                        if (confirm === "Yes") {
+                                            //change stock quantity on mysql
+                                            connection.query(
+                                                "UPDATE rockinzon_db.products SET stock_quantity = stock_quantity - 1  WHERE product_name=?",
+                                                [productChosen],
+                                                function(err, res) {
+                                                    if (err) throw err;
+                                                    
+                                                }
+                                            );
+                                        } else {
+                                            initShop();
+                                        }
+                                    });
+                            }
+                        );
+                    });
+            }
+        );
+    }
 });
